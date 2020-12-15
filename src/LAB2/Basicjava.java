ff9170d949f8db42be74890aa1af93ac19cc015a
LabComMT-3 <netsara.b1503@gmail.com>
2020-12-15 09:50:16 +0700

    Data Type integer

---
+++ src/LAB2/Basicjava.java
@@ -0,0 +1,20 @@
+package LAB2;
+
+public class Basicjava {
+    public static void main(String[] args) {
+
+        System.out.println("Data Type in Programming");
+        //Integer จำนวนเต็ม เช่น -100 -2 -1 0 1 2 3 10 100
+        int number;
+            number = 100; //number เก็บค่าข้อมูลจำนวนเต็ม 100
+        System.out.println(number);
+
+
+
+    }//main
+
+
+
+
+
+}//class
